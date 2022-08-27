39b6a0e0372a223cc3c66a1232b3f1f9d4b332cf
Tamanna Akter <96432968+Tasnim63@users.noreply.github.com>
2022-08-27 19:14:00 +0600

    close added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaStream } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
 import { Link, NavLink } from "react-router-dom";
 import { links } from '../data';
 import './navbar.css';
 export default function Navbar() {
+    const [isNavShowing , setIsNavShowing ] =useState(false);
   return (
   <nav>
     <div className='container nav_container'>
        <Link to="/" className='logo'>
         <img src="http://zwin.io/html/gazania/assets/img/logo.png" alt="Nav logo" />
        </Link>
-       <ul className='nav_links'>
+       <ul className={`nav_links ${isNavShowing ?  'show_nav' : 'hide_nav'}`}>
         {
             links.map(({name,path}, inddex)=>{
                 return(
@@ -21,7 +23,12 @@ export default function Navbar() {
             })
         }
        </ul>
-       <button className='nav_toggle_btn'><FaStream></FaStream></button>
+       <button onClick={() => setIsNavShowing(!isNavShowing)} className='nav_toggle_btn'>
+        
+        {
+            isNavShowing ? <AiOutlineClose></AiOutlineClose> :    <FaStream></FaStream>
+        }
+       </button>
     </div>
   </nav>
   )

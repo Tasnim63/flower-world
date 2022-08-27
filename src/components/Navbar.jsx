54d5f734abd806383512c3c8bdf770c03d1a7825
Tamanna Akter <96432968+Tasnim63@users.noreply.github.com>
2022-08-27 13:23:36 +0600

    nav logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-import './navbar.css'
+import React from 'react';
+import { FaStream } from 'react-icons/fa';
+import { Link, NavLink } from "react-router-dom";
+import { links } from '../data';
+import './navbar.css';
 export default function Navbar() {
   return (
   <nav>
@@ -8,6 +10,18 @@ export default function Navbar() {
        <Link to="/" className='logo'>
         <img src="https://htmldemo.net/fultala/p2/img/fultala/logo.png" alt="Nav logo" />
        </Link>
+       <ul className='nav_links'>
+        {
+            links.map(({name,path}, inddex)=>{
+                return(
+                    <li>
+                        <NavLink to={path}>{name}</NavLink>
+                    </li>
+                )
+            })
+        }
+       </ul>
+       <button className='nav_toggle_btn'><FaStream></FaStream></button>
     </div>
   </nav>
   )

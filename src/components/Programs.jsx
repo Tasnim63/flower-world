e3ad8fd58f9e7342e854b34c68adad8c0f242f68
Tamanna Akter <96432968+Tasnim63@users.noreply.github.com>
2022-08-28 09:57:09 +0600

    flowres program added

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import Section_head from './Section_head'
-import {FaCrown } from "react-icons/fa";
-import { programs } from '../data';
-import Card from '../UI/Card';
+import React from "react";
+import Section_head from "./Section_head";
+import { FaCrown } from "react-icons/fa";
+import { AiFillCaretRight } from "react-icons/ai";
+import { programs } from "../data";
+import Card from "../UI/Card";
+import { Link } from "react-router-dom";
 export default function Programs() {
   return (
-   <section className='programs'>
+    <section className="programs">
       <div className="container programs_container">
-      <Section_head icon={<FaCrown></FaCrown>} tittle="Our Flowers Program"></Section_head>
-      </div>
+        <Section_head
+          icon={<FaCrown></FaCrown>}
+          tittle="Our Flowers Program"
+        ></Section_head>
+    
       <div className="program_wrapper">
-        {
-            programs.map(({id,icon,tittle,info,path}) =>{
-                return (
-                  <Card className="programs_program" key={id}>
-<span>{icon}</span>
-<h4>{tittle}</h4>
-<small>{info}</small>
-                  </Card>  
-                )
-            })
-        }
+        {programs.map(({ id, icon, tittle, info, path }) => {
+          return (
+            <Card className="programs_program" key={id}>
+              <span>{icon}</span>
+              <h4>{tittle}</h4>
+              <small>{info}</small>
+              <Link to={path} className=" btn sm">Learn More <AiFillCaretRight></AiFillCaretRight></Link>
+            </Card>
+          );
+        })}
       </div>
-   </section>
-  )
+      </div>
+    </section>
+  );
 }
